862ddf92a57599619f571e24fc95023d27a08429
Stiwar9816 <stiwar.asprilla1998@gmail.com>
2022-05-28 14:25:13 -0500

    change name db

--- dashboard/config/db.php
+++ dashboard/config/db.php
@@ -3,5 +3,5 @@
 define('DB_HOST', 'us-cdbr-east-05.cleardb.net');//DB_HOST:  generalmente suele ser "127.0.0.1"
 define('DB_USER', 'bcb45dc35194c7');//Usuario de tu base de datos
 define('DB_PASS', '519c9f25');//Contraseña del usuario de la base de datos
-define('DB_NAME', 'heroku_14b5e1dc5da7bd40');//Nombre de la base de datos
+define('DB_NAME', 'heroku_14b5e1dc5da7bd4');//Nombre de la base de datos
 ?>
