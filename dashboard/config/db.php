cae08fe0b6445457c86616b4ed10bbc7ffff1927
Stiwar9816 <stiwar.asprilla1998@gmail.com>
2022-05-28 14:19:54 -0500

    config clearDB

--- dashboard/config/db.php
+++ dashboard/config/db.php
@@ -1,7 +1,7 @@
 <?php
 /*Datos de conexion a la base de datos*/
-define('DB_HOST', 'localhost');//DB_HOST:  generalmente suele ser "127.0.0.1"
-define('DB_USER', 'root');//Usuario de tu base de datos
-define('DB_PASS', '');//Contraseña del usuario de la base de datos
-define('DB_NAME', 'simple_stock');//Nombre de la base de datos
+define('DB_HOST', 'us-cdbr-east-05.cleardb.net');//DB_HOST:  generalmente suele ser "127.0.0.1"
+define('DB_USER', 'bcb45dc35194c7');//Usuario de tu base de datos
+define('DB_PASS', '519c9f25');//Contraseña del usuario de la base de datos
+define('DB_NAME', 'heroku_14b5e1dc5da7bd40');//Nombre de la base de datos
 ?>
